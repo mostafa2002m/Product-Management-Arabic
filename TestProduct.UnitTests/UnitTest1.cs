fb397ee370141b4a0748a9aedaa0f5772acfa52a
Ibrahim Mostafa <mostafa2002m@gmail.com>
2018-11-17 19:37:36 +0300

    Signed-off-by: Ibrahim Mostafa <mostafa2002m@gmail.com>

---
+++ TestProduct.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProduct.UnitTests
+{
+    [TestClass]
+    public class ReservasionTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_Scenario_ExpectedBehavior()
+        {
+        }
+    }
+}
